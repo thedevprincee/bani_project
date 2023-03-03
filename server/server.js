4eba15568bebf1cc3341a0d4242557487a8bd1cf
Ochuko George <georgeochuko7@gmail.com>
2023-03-03 10:01:40 +0100

    done with auth and jwt verifier

--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 6000
 const mongoose = require('mongoose')
+const verifyJwt = require('./src/middleware/auth')
 mongoose.set("strictQuery", false)
 mongoose.connect(process.env.CONNECTION_URI, ()=>console.log("DATABASE CONNECTED"))
+
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use('/auth', require('./src/controllers/auth-controller'))
+app.use(verifyJwt)
+
 app.listen(PORT, ()=> console.log(`currently connected at Port ${PORT}`))
